5421008d1b53b912837b9408549739d4b06815e1
吴惠健 <89465005@qq.com>
2018-09-18 19:31:37 +0800

    eidt username

---
+++ kaijiang1/config2.js
@@ -0,0 +1,118 @@
+// 彩票开奖配置
+exports.cp=[
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+{                                                                                                           //
+	title:'开彩网重庆时时彩',                                                                               //
+	source:'开彩网',                                                                                 		//
+	name:'cqssc',                                                                                           //
+	enable:true,                                                                                            //
+	timer:'cqssc1',                                                                                          //
+	option:{                                                                                                //
+		host:"ho.apiplus.net",                                                                              //
+		timeout:50000,                                                                                      //重
+		path: '/daily.do?token=tf84ad5ef02880ec8k&code=cqssc&format=json&date=2018-09-18',                   //庆
+		headers:{                                                                                           //时
+			"User-Agent": "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0) "                             //时
+		}                                                                                                   //彩
+	},                                                                                                      //
+	parse:function(str){                                                                                    //
+		try{                                                                                                //
+			return str.data;                                                                                          //
+		}catch(err){                                                                                        //
+			throw('--------开彩网重庆时时彩解析数据不正确');                                                  //
+		}                                                                                                   //
+	}                                                                                                       //
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+];                                                                                                              
+
+// 出错时等待 15                                                                                                
+exports.errorSleepTime=15;                                                                                      
+
+// 重启时间间隔，以小时为单位，0为不重启
+//exports.restartTime=0.4;
+exports.restartTime=0;
+
+exports.submit={
+
+	host:'localhost',
+	path:'/index.php/dataSource/kj'
+}
+
+exports.dbinfo={
+	host:'localhost',
+	user:'root',
+	password:'',
+	database:'0xc'
+
+}
+
+global.log=function(log){
+	var date=new Date();
+	console.log('['+date.toDateString() +' '+ date.toLocaleTimeString()+'] '+log)
+}
+
+
+function reparseFrom9800(bet, type) {
+	str = bet.str;
+
+	exports.errorSleepTime=500;  
+
+	reg = new RegExp("<TD bgColor=#f6f6f6 align=\"center\"" + bet.actionNo + "<\/TD>[\s\S].*?<TD align=middle>(.*?)<\/TD>[\s\S].*?<TD align=middle>[\s\S].*?<font color=\"#FF0000\"><b>(\d+) (\d+) (\d+) (\d+) (\d+) (\d+)<\/b><\/font>[\s\S].*?\+ <b><font color=\"#009933\">(\d+)<\/font><\/b>", ""); //
+
+	match = str.match(reg);
+	var myDate = new Date();
+	var year = myDate.getFullYear(); //年
+	var month = myDate.getMonth() + 1; //月
+	var day = myDate.getDate(); //日
+	if (month < 10) month = "0" + month;
+	if (day < 10) day = "0" + day;
+	var mytime = match[1] + " " + myDate.toLocaleTimeString();
+	try {
+		var data = {
+			type: type,
+			time: mytime,
+			number: bet.actionNo
+		}
+
+		data.data = match[2] + "," + match[3] + "," + match[4] + "," + match[5] + "," + match[6] + "," + match[7] + "," + match[7];
+
+		//console.log(data);
+		return data;
+	} catch (err) {
+		throw ('解析数据失败');
+	}
+
+}
+
+function getFrom9800(str, type) {
+
+	str = str.substr(str.indexOf('bai12'), 560);
+	exports.errorSleepTime=500;  
+
+	var reg = /<TD bgColor=#f6f6f6 align="center">(\d+)<\/TD>[\s\S].*?<TD align=middle>(.*?)<\/TD>[\s\S].*?<TD align=middle>[\s\S].*?<font color="#FF0000"><b>(\d+) (\d+) (\d+) (\d+) (\d+) (\d+)<\/b><\/font>[\s\S].*?\+ <b><font color="#009933">(\d+)<\/font><\/b>/,
+		match = str.match(reg);
+
+	var myDate = new Date();
+	var year = myDate.getFullYear(); //年
+	var month = myDate.getMonth() + 1; //月
+	var day = myDate.getDate(); //日
+	if (month < 10) month = "0" + month;
+	if (day < 10) day = "0" + day;
+	var mytime = match[2] + " " + myDate.toLocaleTimeString();
+	try {
+		var data = {
+			type: type,
+			time: mytime,
+			number: match[1]
+		}
+
+		data.data = match[3] + "," + match[4] + "," + match[5] + "," + match[6] + "," + match[7] + "," + match[8] + "," + match[9];
+
+		//console.log(data);
+		return data;
+	} catch (err) {
+		throw ('解析数据失败');
+	}
+
+}
