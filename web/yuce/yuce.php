5b5fc13e5e5fa70634f7b66d2c3525e9c16276f1
wuhuijian <89465005@qq.com>
2018-09-24 11:55:19 +0800

    a

--- web/yuce/yuce.php
+++ web/yuce/yuce.php
@@ -104,13 +104,9 @@ function clac($oldData,$newId,$newData,$oldId,$qihao,$mydb,$conf){
         $index = 1;
         foreach ($all23Datas as $value){
             $oldData[$value]["currResult"] = $index;
-            $oldData[$value]["nextProfit"] = 1;
-            $oldData[$value]["nextRound"] = 1;
-            $oldData[$value]["nextBeishu"] = 1;
-            $oldData[$value]["beishuIndex"] = 0;
-              if($index==3){
-                  $oldData[$value]["currResult"] = 0;
-              }
+            if($index==3){
+                $oldData[$value]["currResult"] = 0;
+            }
             $index++;
         }
         $whereStr = " id=$oldId";
