4c0f2adf82346b8fe964cd2dc3ff7dc29a32a216
吴惠健 <89465005@qq.com>
2018-10-12 11:48:53 +0800

    login page

--- kaijiang1/kj.sh
+++ kaijiang1/kj.sh
@@ -1,1 +1,3 @@
-nohup node /www/wwwroot/wap.dianjinshuju.com/kaijiang1/data2.js > /www/wwwroot/wap.dianjinshuju.com/kaijiang1/run2.log 2>&1 &
+#!/bin/bash
+ps aux | grep "p "data2.js" |g" |grep -v grep| cut -c 9-15 | xargs kill -9 2>/dev/null
+nohup node /www/wwwroot/dianjinshuju.com/kaijiang1/data2.js > /www/wwwroot/dianjinshuju.com/kaijiang1/run2.log 2>&1 &
